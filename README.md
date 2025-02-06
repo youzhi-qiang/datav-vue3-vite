be2a4a25eba99b74e4f9393b5fa61a0840a22c26
youzq <yzq1024aswater@163.com>
2025-02-06 13:10:09 +0800

    element-plus表格适配实验

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # datav-vue3-vite
 使用vue3+vite实现数据大屏各种分辨率屏幕适配
+node 版本 20.17.0
+pnpm i
+pnpm run dev
--- src/views/IndexData6.vue
+++ src/views/IndexData6.vue
@@ -1,66 +1,43 @@
 <template>
-    <dv-loading v-if="loading1"></dv-loading>
-    <div ref="refecharts" class="interior" v-else>
-    </div>
+   <dv-loading v-if="loading1"></dv-loading>
+     <!-- <div ref="refecharts" class="interior" v-else>
+    </div> -->
+    <el-table :data="tableData" class="interior" v-else>
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
 </template>
 
-<script lang="ts">
-import { info3 } from "./api"
-import { ref, onMounted, nextTick, watch } from "vue"
-import * as echarts from 'echarts';
+<script lang="ts" setup>
+import { ref } from 'vue';
 
-export default {
-    setup() {
-        const loading1 = ref(true)
-        const refecharts = ref()
-        const echartsfun = ({data}: any) => {
-     
-            const myChart = echarts.init(refecharts.value);
-            const option = {
-                xAxis: {
-                    type: 'category',
-                    data: Object.keys(data)
-                },
-                yAxis: {
-                    type: 'value'
-                },
-                series: [
-                    {
-                        data: Object.values(data),
-                        type: 'bar'
-                    }
-                ]
-            };
+const loading1 = ref(false)
 
-            option && myChart.setOption(option);
-        }
-        const getdata = async () => {
-            const { data } = await info3()
-            if (data.code == 200) {
-                loading1.value = false
-                nextTick(() => {  //当loadging切换时，dom变化，执行nexttick函数
-                    if (refecharts.value) {  //判断是否获取到dom，如果没有获取到就不执行，只执行onmounted里的getdata，防止调试时因为获取不到dom而报错
-                        echartsfun(data)
-                    }
-                })
-
-            }
-
-        }
-
-        onMounted(() => {
-            setInterval(() => {
-                getdata()
-            }, 360000)
-            getdata()
-        })
-
-        return {
-            refecharts, loading1
-        }
-    }
-}
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 </script>
+ 
 
 <style scoped>
 .interior {
